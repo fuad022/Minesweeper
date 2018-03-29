96942dc079c09cc6dedf768a0b0d68119052cebf
Fuad Malikov <fuad.malikov@knexel.com>
2018-03-29 22:04:55 +0400

    First GitHub project.

---
+++ src/Algorithms/Algorithms.java
@@ -0,0 +1,13 @@
+package Algorithms;
+
+public class Algorithms {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i<=100; i++) {
+            System.out.println(i);
+        }
+        main(args);
+    }
+
+}
