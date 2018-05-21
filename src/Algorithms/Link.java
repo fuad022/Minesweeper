abaab464987c515d501e2195db70be16023217a8
Fuad Malikov <fuad.malikov@knexel.com>
2018-05-21 21:03:11 +0400

    Links example.

---
+++ src/Algorithms/Link.java
@@ -0,0 +1,62 @@
+package Algorithms;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Link {
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+//        List<Person> arrayList = new ArrayList<>();
+
+        Person person1 = new Person("Wolf");
+        Person person2 = new Person("Fox");
+        Person person3 = new Person("Bunny");
+
+        
+
+
+
+//        arrayList.add(person1);
+//        arrayList.add(person2);
+//        arrayList.add(person3);
+
+//        Iterator<Person> iterator = arrayList.iterator();
+
+//        System.out.println();
+
+        
+
+//        for (int i = 0; i < 1000000; i++) {
+//            arrayList.add(i);
+//            linkedList.add(i);
+//        }
+//
+//        long arrayListTime = System.currentTimeMillis();
+//
+////        arrayList.add(500, 1203);
+////        arrayList.remove(500);
+////        arrayList.get(100000);
+////        Iterator<Integer> iterator = arrayList.iterator();
+////        System.out.println(iterator);
+//
+//        long arrayResultTime = System.currentTimeMillis() - arrayListTime;
+//
+//        System.out.println("ArrayList time: " + arrayResultTime);
+//
+//        long linkedListTime = System.currentTimeMillis();
+//
+////        linkedList.add(500, 1203);
+////        linkedList.remove(500);
+//        linkedList.get(100000);
+//
+//        long linkedResultTime = System.currentTimeMillis() - linkedListTime;
+//
+//        System.out.println("LinkedList time: " + linkedResultTime);
+
+    }
+
+}
