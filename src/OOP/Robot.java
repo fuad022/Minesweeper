da9f4ca64d346efb80d91e45c8bf0f93cd0ac1e8
Fuad Malikov <fuad.malikov@knexel.com>
2018-07-29 12:58:07 +0400

    Changes added.

--- src/OOP/Robot.java
+++ src/OOP/Robot.java
@@ -3,23 +3,40 @@ package OOP;
 public class Robot {
 
     //Текущая координата X
-    double x = 0;
+    private double x = 0;
     //Текущая координата Y
-    double y = 0;
+    private double y = 0;
     //Текущий курс (в градусах)
-    double course = 0;
-    //Печать координат робота
-    void printCoordinates() {
-        System.out.println(x + ", " + y);
-    }
-
+    private double course = 0;
+    
     // Передвижение на дистанцию distance
     // учтите, что угол надо передавать в радианах. Для того, что бы работать в градусах
     // мы преобразуем градусы в радианы путем деления на 180 и умножением на PI (Пи - 3,14159)
-    void forward(int distance) {
+    public void forward(int distance) {
         //Обращение к полю объекта X
         x = x + distance * Math.cos(course / 180 * Math.PI);
         // Обращение к полю объекта Y
         y = y + distance * Math.sin(course / 180 * Math.PI);
+    }
+
+    //Печать координат робота
+    public void printCoordinates() {
+        System.out.println(x + ", " + y);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getCourse() {
+        return course;
+    }
+
+    public void setCourse(double course) {
+        this.course = course;
     }
 }
